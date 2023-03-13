62d2d545094ad8270bc48ff12e7a268d48bbdf99
AVCOE <tusharargade147@mail.com>
2023-03-13 15:14:04 +0530

    Balaji

---
+++ Binary.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a[15], n,i, low=0, high, mid,flag=0;
+	
+	cout<<"\n TOTAL NO. OF STUDENTS:";
+	cin>>n;
+	
+	cout<<"\n ENTER ROLL NO. IN ASSENDING ORDER:"; 
+	for(i=0;i<0;i++)
+	{
+	cin>>n>>a[i];
+	}
+	cout<<"\n ROLL NO. TO BE FIND:";
+	cin>>n;
+	
+	high=n-1
+	while (low<=high)
+	{
+		if(n=a[mid])
+		{
+		 flag==1;
+		 brek;
+		} 
+		  
+	}
+	if(n<a[i])
+	{
+		low=mid+1;
+	}
+	
+	else 
+	{
+		high= mid-1;
+	}
+	
+	
+	mid=(low=high)/2;
+	
+	
+	if
+	{
+		flag==1;
+		cout<<"\n Required roll no. is Found";
+	}
+	else
+	{
+		cout<<"\n NOT FOUND";
+	}
+}	
+			
+		
+	
